cd48becfb8eb49ba66999c28d75c00d485a5cd7e
amedeo <amedeo@diflexmo.be>
2023-01-24 18:50:28 +0100

    add tooltip on ellipsis in table

--- projects/diflexmo-angular-design/src/lib/design-system/modules/data-table/components/table-row-cell/table-row-cell.component.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/modules/data-table/components/table-row-cell/table-row-cell.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import { Tooltip } from 'bootstrap';
 
 @Component({
   selector: 'dfm-table-row-cell',
   templateUrl: './table-row-cell.component.html',
   styleUrls: ['./table-row-cell.component.scss']
 })
-export class TableRowCellComponent {
-
+export class TableRowCellComponent implements AfterViewInit, OnDestroy {
+  
   @Input() propagateClick: boolean = true;
 
   @Input() contentAlign: 'right' | 'left' = 'left';
@@ -15,9 +16,31 @@ export class TableRowCellComponent {
 
   @Input() maxWidthStyle?: string;
 
+  @ContentChild('projected', { static: false }) projectedContent: any;
+
+  @ViewChild('tableCell', { read: ElementRef }) tableCell!: ElementRef;
+
+  private tooltips: Tooltip[] = [];
+
+  ngAfterViewInit(): void {
+    if (this.isEllipsisActive(this.projectedContent.nativeElement.innerHTML)) {
+      const copyTooltip = new Tooltip(this.tableCell.nativeElement, { title: this.projectedContent.nativeElement.innerHTML });
+
+      this.tooltips.push(copyTooltip);
+    }
+  }
+
   public propagateEvent(clickEvent: any): void {
     if (!this.propagateClick) {
       clickEvent.stopPropagation();
     }
+  }
+
+  ngOnDestroy(): void {
+    this.tooltips.forEach((t) => t.dispose());
+  }
+
+  isEllipsisActive(e: any) {
+    return (e.offsetWidth < e.scrollWidth);
   }
 }
