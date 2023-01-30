f1f89259a690cc9548353fd3b6dac258dde40543
amedeo <amedeo@diflexmo.be>
2023-01-30 17:07:23 +0100

    remove obsolete

--- projects/diflexmo-angular-design/src/lib/design-system/modules/dropdown/components/dropdown-button/dropdown-button.component.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/modules/dropdown/components/dropdown-button/dropdown-button.component.ts
@@ -1,17 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'dfm-dropdown-button',
   templateUrl: './dropdown-button.component.html',
   styleUrls: ['./dropdown-button.component.scss']
 })
-export class DropdownButtonComponent implements OnInit {
+export class DropdownButtonComponent {
 
   useEllipsis: boolean = true;
   
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
