def82dcf7e39f298a40916b592d0e7788f16194d
amedeo <amedeo@diflexmo.be>
2023-01-31 13:58:50 +0100

    fix form issue in data table

--- projects/diflexmo-angular-design/src/lib/design-system/modules/data-table/data-table.module.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/modules/data-table/data-table.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AngularResizeEventModule } from 'angular-resize-event';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { DataTableComponent } from './components/data-table/data-table.component';
 import { IconModule } from '../icon/icon.module';
 import { TableRowCellComponent } from './components/table-row-cell/table-row-cell.component';
@@ -12,7 +13,7 @@ import { BaseCheckboxModule } from '../base-checkbox/base-checkbox.module';
 
 @NgModule({
   declarations: [DataTableComponent, TableRowCellComponent, DataTableHeaderCellComponent],
-  imports: [CommonModule, IconModule, AngularResizeEventModule, ButtonModule, NgDfmDropdownModule, NgbTooltipModule, BaseCheckboxModule],
+  imports: [CommonModule, IconModule, AngularResizeEventModule, ButtonModule, NgDfmDropdownModule, NgbTooltipModule, FormsModule, BaseCheckboxModule],
   exports: [DataTableComponent, TableRowCellComponent],
 })
 export class DfmDataTableModule {}
