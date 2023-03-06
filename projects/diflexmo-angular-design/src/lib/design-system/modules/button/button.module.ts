109e235b3e62f49df2ccd17e362df9fb5ab6566e
Amedeo <amedeo@diflexmo.be>
2023-03-06 20:09:52 +0100

    add button directive

--- projects/diflexmo-angular-design/src/lib/design-system/modules/button/button.module.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/modules/button/button.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './components/button/button.component';
+import { ButtonComponent, NgDfmButtonComponent } from './components/button/button.component';
 import { NgDfmIconModule } from '../icon/icon.module';
 import { ButtonGroupComponent } from './components/button-group/button-group.component';
 import { ButtonIconComponent } from './components/button-icon/button-icon.component';
 import { ButtonGroupItemComponent } from './components/button-group-item/button-group-item.component';
 
 @NgModule({
-  declarations: [ButtonComponent, ButtonGroupComponent, ButtonIconComponent, ButtonGroupItemComponent],
+  declarations: [ButtonComponent, ButtonGroupComponent, ButtonIconComponent, ButtonGroupItemComponent, NgDfmButtonComponent],
   imports: [CommonModule, NgDfmIconModule],
-  exports: [ButtonComponent, ButtonGroupComponent, ButtonGroupItemComponent, ButtonIconComponent],
+  exports: [ButtonComponent, ButtonGroupComponent, ButtonGroupItemComponent, ButtonIconComponent, NgDfmButtonComponent],
 })
 export class NgDfmButtonModule {}
--- projects/diflexmo-angular-design/src/lib/design-system/modules/button/components/button/button.component.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/modules/button/components/button/button.component.ts
@@ -24,3 +24,27 @@ export class ButtonComponent {
     this.clicked.emit();
   }
 }
+
+@Component({
+  selector: 'button[dfm-button]',
+  styleUrls: ['./dfm-button.scss'],
+  template: '<ng-content></ng-content>',
+  inputs: ['disabled'],
+  host: {
+    '[attr.disabled]': 'disabled',
+    '[class.dfm-py-8]': 'size === sm',
+    '[class.dfm-py-10]': 'size === md || size === lg',
+    '[class.dfm-py-12]': 'size === xl',
+    '[class.dfm-py-16]': 'size === 2xl',
+    '[class.dfm-px-14]': 'size === sm',
+    '[class.dfm-px-16]': 'size === md',
+    '[class.dfm-px-18]': 'size === lg',
+    '[class.dfm-px-20]': 'size === xl',
+    '[class.dfm-px-28]': 'size === 2xl'
+  }
+})
+export class NgDfmButtonComponent {
+  @Input() color: ButtonColor = 'primary';
+
+  @Input() size: ButtonSize = 'lg';
+}
