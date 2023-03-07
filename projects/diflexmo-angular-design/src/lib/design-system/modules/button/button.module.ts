ee73e4b8b95bba8060b5c8a6ede0ed8e52d599c5
Amedeo <amedeo@diflexmo.be>
2023-03-07 11:41:46 +0100

    remove exports of 2nd button type

--- projects/diflexmo-angular-design/src/lib/design-system/modules/button/button.module.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/modules/button/button.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent, NgDfmButtonComponent } from './components/button/button.component';
+import { ButtonComponent } from './components/button/button.component';
 import { NgDfmIconModule } from '../icon/icon.module';
 import { ButtonGroupComponent } from './components/button-group/button-group.component';
 import { ButtonIconComponent } from './components/button-icon/button-icon.component';
 import { ButtonGroupItemComponent } from './components/button-group-item/button-group-item.component';
 
 @NgModule({
-  declarations: [ButtonComponent, ButtonGroupComponent, ButtonIconComponent, ButtonGroupItemComponent, NgDfmButtonComponent],
+  declarations: [ButtonComponent, ButtonGroupComponent, ButtonIconComponent, ButtonGroupItemComponent],
   imports: [CommonModule, NgDfmIconModule],
-  exports: [ButtonComponent, ButtonGroupComponent, ButtonGroupItemComponent, ButtonIconComponent, NgDfmButtonComponent],
+  exports: [ButtonComponent, ButtonGroupComponent, ButtonGroupItemComponent, ButtonIconComponent],
 })
 export class NgDfmButtonModule {}
