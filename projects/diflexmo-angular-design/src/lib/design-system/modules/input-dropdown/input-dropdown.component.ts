069b8d3f75226da56a9f170168795daa0469b1ce
Amedeo <amedeo@diflexmo.be>
2023-04-25 16:19:03 +0200

    hide dropdown on focus loss

--- projects/diflexmo-angular-design/src/lib/design-system/modules/input-dropdown/input-dropdown.component.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/modules/input-dropdown/input-dropdown.component.ts
@@ -298,6 +298,7 @@ export class InputDropdownComponent extends BaseControlValueAccessor implements 
   }
 
   public focusout(value: any) {
+    this.dropdownInstance?.hide();
     setTimeout(() => {
       this.onTouch(value);
     }, 250);
