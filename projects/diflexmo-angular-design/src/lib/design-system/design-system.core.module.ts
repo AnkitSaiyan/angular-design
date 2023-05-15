c001c688cb236b4696b241aac840145c9e01b72b
Amedeo <amedeo@diflexmo.be>
2023-05-15 14:57:04 +0200

    remove sefl check from core module

--- projects/diflexmo-angular-design/src/lib/design-system/design-system.core.module.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/design-system.core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, SkipSelf } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { IconCoreModule } from './modules/icon/icon.core.module';
 import { PipesModule } from './pipes/pipes.module';
 
@@ -6,10 +6,4 @@ import { PipesModule } from './pipes/pipes.module';
   imports: [IconCoreModule, PipesModule],
   exports: [PipesModule],
 })
-export class DesignSystemCoreModule {
-  constructor(@SkipSelf() coreModule: DesignSystemCoreModule) {
-    if (!coreModule) {
-      throw new Error('DfmCoreModule should only be imported once in the app module');
-    }
-  }
-}
+export class DesignSystemCoreModule {}
