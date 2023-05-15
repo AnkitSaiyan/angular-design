39c77e92e3f0ac0eae52e8bb8e71f1d1cbd7413e
Amedeo <amedeo@diflexmo.be>
2023-05-15 15:38:45 +0200

    add logging to localized pipe

--- projects/diflexmo-angular-design/src/lib/design-system/pipes/dfm-localized-date.pipe.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/pipes/dfm-localized-date.pipe.ts
@@ -12,6 +12,7 @@ export class DfmLocalizedDatePipe extends DatePipe implements PipeTransform {
   }
 
   public override transform(value: Date | string | number | null | undefined, format?: string, timezone?: string, locale?: string) {
+    console.log('test', value);
     const dateLocale = locale ?? this.localizationService.getCurrentLocale();
     return super.transform(value, format, timezone, dateLocale) as any;
   }
