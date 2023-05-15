44b5b542e946489b7de959d844a74022a4a6c768
Amedeo <amedeo@diflexmo.be>
2023-05-15 15:15:56 +0200

    add register locales for nl

--- projects/diflexmo-angular-design/src/lib/design-system/design-system.core.module.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/design-system.core.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
-import { IconCoreModule } from './modules/icon/icon.core.module';
+import { registerLocaleData } from '@angular/common';
+import localeNlBe from '@angular/common/locales/nl-BE';
+import localeNl from '@angular/common/locales/nl';
 import { PipesModule } from './pipes/pipes.module';
+import { IconCoreModule } from './modules/icon/icon.core.module';
+
+registerLocaleData(localeNl);
+registerLocaleData(localeNlBe);
 
 @NgModule({
   imports: [IconCoreModule, PipesModule],
