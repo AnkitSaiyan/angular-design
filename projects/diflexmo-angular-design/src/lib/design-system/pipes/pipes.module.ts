647ca141137840a4b52945f623443be6653e5646
Ankit Kumar <ankit.kumar@remotestate.com>
2023-05-25 17:58:52 +0530

    Localization changes in navigation bar, input error messages and input dropdown

--- projects/diflexmo-angular-design/src/lib/design-system/pipes/pipes.module.ts
+++ projects/diflexmo-angular-design/src/lib/design-system/pipes/pipes.module.ts
@@ -4,11 +4,12 @@ import { DfmLocalizedDatePipe } from './dfm-localized-date.pipe';
 import { DfmLocalizedCurrencyPipePipe } from './dfm-localized-currency-pipe.pipe';
 import { DfmLocalizedDecimalPipePipe } from './dfm-localized-decimal-pipe.pipe';
 import { DfmLocalizedPercentPipePipe } from './dfm-localized-percent-pipe.pipe';
+import { DfmLocalizationPipe } from "./dfm-localization.pipe";
 
 @NgModule({
-  declarations: [DfmLocalizedDatePipe, DfmLocalizedCurrencyPipePipe, DfmLocalizedDecimalPipePipe, DfmLocalizedPercentPipePipe],
+  declarations: [DfmLocalizedDatePipe, DfmLocalizedCurrencyPipePipe, DfmLocalizedDecimalPipePipe, DfmLocalizedPercentPipePipe, DfmLocalizationPipe],
   imports: [CommonModule],
   providers: [DatePipe, CurrencyPipe, DecimalPipe],
-  exports: [DfmLocalizedDatePipe, DfmLocalizedCurrencyPipePipe, DfmLocalizedDecimalPipePipe, DfmLocalizedPercentPipePipe],
+  exports: [DfmLocalizedDatePipe, DfmLocalizedCurrencyPipePipe, DfmLocalizedDecimalPipePipe, DfmLocalizedPercentPipePipe, DfmLocalizationPipe],
 })
 export class DfmPipesModule {}
